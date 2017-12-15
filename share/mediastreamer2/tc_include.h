cba92be6ba003169e3876e3a14d86dae4a4dc43c
bill611 <bill4regist@163.com>
2017-12-15 09:11:06 +0800

    修改3100文件夹名称

--- share/mediastreamer2/tc_include.h
+++ share/mediastreamer2/tc_include.h
@@ -1,59 +1,26 @@
-#ifdef TC3000_Z1
-#include "z1_mh/config.h"
-#include "z1_mh/tc_protocol/public.h"
-#include "z1_mh/tc_protocol/protocol.h"
+#if (defined TC3000_Z1) \
+|| (defined TC3000_T1) \
+|| (defined TC3000_R1) \
+|| (defined TC3000_W2) \
+|| (defined TC3000_W1) \
+|| (defined TC3000_X1)
+
+#include "3000_indoor_no_touch/config.h"
+#include "3000_indoor_no_touch/tc_protocol/public.h"
+#include "3000_indoor_no_touch/tc_protocol/protocol.h"
 #endif
-#ifdef TC3000_Q1
-#include "q1_mh/config.h"
-#include "q1_mh/tc_protocol/public.h"
-#include "q1_mh/tc_protocol/protocol.h"
+
+
+#if (defined TC3000_Z1_CM) \
+|| (defined TC3000_18D1) \
+|| (defined TC3000_W2_CM) \
+|| (defined TC3000_W1_CM)
+#include "3000_indoor_touch/config.h"
+#include "3000_indoor_touch/tc_protocol/public.h"
+#include "3000_indoor_touch/tc_protocol/protocol.h"
 #endif
-#ifdef TC3000_T1
-#include "t1_mh/config.h"
-#include "t1_mh/tc_protocol/public.h"
-#include "t1_mh/tc_protocol/protocol.h"
-#endif
-#ifdef TC3000_X1
-#include "x1_mh/config.h"
-#include "x1_mh/tc_protocol/public.h"
-#include "x1_mh/tc_protocol/protocol.h"
-#endif
-#ifdef TC3000_R1
-#include "r1_mh/config.h"
-#include "r1_mh/tc_protocol/public.h"
-#include "r1_mh/tc_protocol/protocol.h"
-#endif
-#ifdef TC3000_W2
-#include "w2_mh/config.h"
-#include "w2_mh/tc_protocol/public.h"
-#include "w2_mh/tc_protocol/protocol.h"
-#endif
-#ifdef TC3000_W1
-#include "w1_mh/config.h"
-#include "w1_mh/tc_protocol/public.h"
-#include "w1_mh/tc_protocol/protocol.h"
-#endif
-#ifdef TC3000_Z1_CM
-#include "z1_cm_mh/config.h"
-#include "z1_cm_mh/tc_protocol/public.h"
-#include "z1_cm_mh/tc_protocol/protocol.h"
-#endif
-#ifdef TC3000_18D1
-#include "18d1_mh/config.h"
-#include "18d1_mh/tc_protocol/public.h"
-#include "18d1_mh/tc_protocol/protocol.h"
-#endif
-#ifdef TC3000_W2_CM
-#include "w2_cm_mh/config.h"
-#include "w2_cm_mh/tc_protocol/public.h"
-#include "z1_mh/tc_protocol/protocol.h"
-#endif
-#ifdef TC3000_W1_CM
-#include "w1_cm_mh/config.h"
-#include "w1_cm_mh/tc_protocol/public.h"
-#include "z1_mh/tc_protocol/protocol.h"
-#endif
-#ifdef TC3100_18D1
-#include "3100_18d1/config.h"
-#include "3100_18d1/tc_protocol/protocol.h"
+
+#if (defined TC3100_18D1)
+#include "3100_indoor_touch/config.h"
+#include "3100_indoor_touch/tc_protocol/protocol.h"
 #endif
